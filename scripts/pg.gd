c662c96788980f58d782109beaca85f184b4f93a
Poppo9 <109960475+Poppo9@users.noreply.github.com>
2025-02-17 19:36:02 +0100

    pg: si muove meglio

--- scripts/pg.gd
+++ scripts/pg.gd
@@ -3,19 +3,32 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 #const JUMP_VELOCITY = -400.0
-
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+var last_direction = "s"
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	#if not is_on_floor():
-		#velocity += get_gravity() * delta
-
-	## Handle jump.
-	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		#velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	# Funziona ma non mi piace
+	#var direction := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var direction = Vector2.ZERO
+	
+	if Input.is_action_pressed("ui_right"):
+		direction.x = 1
+		animated_sprite.play("walk_d")
+		last_direction = "d"
+	elif Input.is_action_pressed("ui_left"):
+		direction.x = -1
+		animated_sprite.play("walk_a")
+		last_direction = "a"
+	elif Input.is_action_pressed("ui_up"):
+		direction.y = -1
+		animated_sprite.play("walk_w")
+		last_direction = "w"
+	elif Input.is_action_pressed("ui_down"):
+		direction.y = 1
+		animated_sprite.play("walk_s")
+		last_direction = "s"
+	else:
+		animated_sprite.play("idle_" + last_direction)
+	
 	velocity = direction * SPEED
 	move_and_slide()
