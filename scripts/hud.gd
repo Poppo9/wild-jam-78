26d8a5ee17a0a251a955a73625ff474e2dfff819
Poppo9 <109960475+Poppo9@users.noreply.github.com>
2025-02-20 21:42:40 +0100

    quests

--- scripts/hud.gd
+++ scripts/hud.gd
@@ -4,8 +4,15 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$days.text = "Day: " + str(Global.giorno)
-	$hour.text = "Hour: " + str(Global.ora)
+	$hour.text = str(Global.ora).lpad(2,"0") + ":" + str(Global.minuto).lpad(2,"0")
 	$quest.text = "Quest: " + str(Global.quest)
+	$HP.text = str(Global.vita) + " HP"
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	pass
+
+func update_quest():
+	
+	Global.quest = Global.quests[randi_range(0, len(Global.quests))]
 	pass
