c815a6de3dd159c560d9039d90a41932b67fe351
Poppo9 <109960475+Poppo9@users.noreply.github.com>
2025-02-21 16:06:14 +0100

    Aggiunte logiche per quest

--- scripts/hud.gd
+++ scripts/hud.gd
@@ -3,14 +3,14 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	pass
+	
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
 	$days.text = "Day: " + str(Global.giorno)
 	$hour.text = str(Global.ora).lpad(2,"0") + ":" + str(Global.minuto).lpad(2,"0")
 	$quest.text = "Quest: " + str(Global.quest)
 	$HP.text = str(Global.vita) + " HP"
-	
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
 
 func update_quest():
 	
---
+++ scripts/quest.gd
@@ -0,0 +1,36 @@
+extends Node2D
+
+@onready var quest: Node2D = $"."
+@onready var parent_node: Node2D = $".."
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	var lista_quest = ["stanza_gatto","corridoio","cucina"] # TODO lista quest attive
+	if body.name == "CharacterBody2D":
+		if parent_node.name in lista_quest: 
+			quest.queue_free()
+			completa_quest(parent_node.name,lista_quest)
+	print(lista_quest)
+
+
+func completa_quest(quest: String, lista_quest):
+	Global.vita -=1 #TODO
+	print("rimuovo "+ quest + " da " + array_to_string(lista_quest))
+	var index = lista_quest.find(quest) 
+	if quest in lista_quest:
+		lista_quest.remove_at(index)
+	
+func array_to_string(arr: Array) -> String:
+	var s = ""
+	for i in arr:
+		s += String(i)
+	return s
